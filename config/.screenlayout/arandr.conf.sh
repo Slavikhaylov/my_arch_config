b2d32e24d33b23717231549bceb998b2408f3f7c
slavikhaylov <hslavik627@gmail.conf>
2023-11-12 16:26:17 +0500

    first commit

---
+++ config/.screenlayout/arandr.conf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x435 --rotate normal --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP-1-1 --off
